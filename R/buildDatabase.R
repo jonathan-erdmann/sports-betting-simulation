c0d082aa53caf4317c1ba7f7e40aa74efc21ee47
Jonathan Erdmann <76535224+jonathan-erdmann@users.noreply.github.com>
2023-04-01 15:46:58 -0500

    Adding web scraping prototypes

---
+++ R/buildDatabase.R
@@ -0,0 +1,185 @@
+#-- Create Betting Simulation Database
+library(DBI)
+library(RSQLite)
+library(data.table)
+
+# Running from sports-betting-simulation/R
+# Database location: sports-betting-simulation/db
+# Data location: sports-betting-simulation/data
+
+dbFileName <- "bettingSimulation.sqlite"
+dbPath <- file.path("C:","Users","Jonathan","Documents","Jonathan","r-projects","sports-betting-simulation", "db")
+dbFile <- paste0(dbPath,"/" ,dbFileName)
+
+#-- Actions
+createSchema <- 0
+populateData <- 1
+
+#Connect to database
+mydb <- dbConnect(RSQLite::SQLite(), dbFile)
+
+#-- Create schema
+if (createSchema) {
+  
+  # Create Bets table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS bets
+    (
+       id integer primary key
+      ,league_id integer
+      ,date text
+      ,team_id integer
+      ,win_probability real
+      ,moneyline integer
+    )
+  ")
+  
+  # Create Leagues table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS leagues
+    (
+       id integer primary key
+      ,name text
+      ,sport text
+      ,league text
+    )
+  ")
+  
+  # Create Teams table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS teams
+    (
+       id integer primary key
+      ,league_id integer
+      ,name text
+      ,team text
+    )
+  ")
+  
+  # Create Outcomes table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS outcomes
+    (
+       id integer primary key
+      ,bets_id integer
+      ,team_id integer
+      ,win integer
+      ,margin integer
+    )
+  ")
+  
+  # Create Risk Configuration table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS risk_config
+    (
+       id integer primary key
+      ,total_bet_max real
+      ,ind_bet_min real
+      ,ind_bet_max real
+      ,n_bets_min integer
+      ,starting_working_capital real
+      ,starting_reserve_capital real
+      ,min_working_to_replenish real
+      ,min_working_to_bank_no_replenish real
+      ,min_working_to_bank_with_replenish real
+      ,min_total_capital_to_rebalance real
+    )
+  ")
+  
+  # Create Simulation Config table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS simulation_config
+    (
+       id integer primary key
+      ,risk_configuraiton integer
+      ,allocation_id integer
+      ,playable_leagues text
+      ,simulation_days integer
+      ,n_simulation_runs integer
+      ,random_seed integer
+    )
+  ")
+  
+  # Create Simulated Potential Bets table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS simulation_potential_bets
+    (
+       simulation_id integer
+      ,simulation_bet_id integer
+      ,betting_day integer
+      ,team_id integer
+      ,win_probability real
+      ,money_line real
+    )
+  ")
+  
+  # Create Simulated Wagered Bets table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS simulation_wagered_bets
+    (
+       simulation_id integer
+      ,simulation_bet_id integer
+      ,betting_day integer
+      ,percentage_of_capital real
+    )
+  ")
+  
+  # Create Simulated Capital Values table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS simulation_capital_values
+    (
+       simulation_id integer
+      ,simulation_bet_id integer
+      ,run integer
+      ,wagered_capital real
+      ,returned_capital real
+      ,profit_loss real
+    )
+  ")
+  
+  # Create Simulation Summaries table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS simulation_summary
+    (
+       simulation_id integer
+      ,number_of_runs integer
+      ,probability_of_win real
+      ,estimated_geometric_return real
+      ,estimated_geometric_return_interval_25 real
+      ,estimated_geometric_return_interval_45 real
+      ,estimated_geometric_return_interval_475 real
+    )
+  ")
+  
+  # Create Capital Allocation Method table
+  dbExecute(mydb, "
+    CREATE TABLE IF NOT EXISTS capital_allocation_method
+    (
+       id integer
+      ,number_of_runs integer
+      ,method text
+    )
+  ")
+  
+}
+
+if (populateData) {
+  
+  #-- Data Location
+  filePath <- file.path("C:","Users","Jonathan","Documents","Jonathan","r-projects","sports-betting-simulation", "data")
+  
+  #-- Load Leagues
+  fileName <- "leagues.csv"
+  csvFile  <- paste0(filePath,"/" ,fileName)
+  xfer_df  <- fread(csvFile)
+  #dbWriteTable(mydb, "leagues", xfer_df, overwite = TRUE, append = TRUE)
+  
+  #-- Load Teams
+  fileName <- "teams.csv"
+  csvFile  <- paste0(filePath,"/" ,fileName)
+  xfer_df  <- fread(csvFile)
+  #dbWriteTable(mydb, "teams", xfer_df, overwite = TRUE, append = TRUE)
+  
+}
+
+dbDisconnect(mydb)
---
+++ R/scrapeBettingData.R
@@ -0,0 +1,123 @@
+library(rvest)
+library(dplyr)
+
+#-- Return NHL Win Probabilities
+get_nhl_win_probabilities <- function() {
+ 
+  #-- Source URL
+  url <- "https://projects.fivethirtyeight.com/2023-nhl-predictions/games/"
+  
+  #-- Note that format of NHL predictions differs, includes repetitions and does not have an easy to fetch date tag for each game
+  #-- Data frame requires post-function filtering
+  
+  webpage <- read_html(url)
+  
+  #-- Get Team Names
+  team_name <- webpage %>% html_nodes(".team-name") %>% html_text()
+  
+  #-- Get Win Probabilities
+  chance <- webpage %>% html_nodes(".num.win-prob") %>% html_text()
+  chance <- as.numeric(sub("%","",chance)) / 100
+  
+  #-- League ID
+  league_id <- 4
+  league <- rep(league_id, length(chance))
+  
+  #-- Return Probabilities after removing duplicates
+  nhl_win_probabilities <- data.frame(league, team_name, chance) %>% distinct(league, team_name, chance, .keep_all = TRUE)
+  
+  return(nhl_win_probabilities)
+  
+}
+
+#-- Return NBA Win Probabilities
+get_mlb_win_probabilities <- function() {
+  
+  #-- Source URL
+  url <- "https://projects.fivethirtyeight.com/2023-mlb-predictions/games/"
+  
+  webpage <- read_html(url)
+  
+  #-- Today's Date in 538 Format for filtering larger games table
+  short_date <- paste0(as.numeric(format(Sys.Date(),"%m")),"/",as.numeric(format(Sys.Date(),"%d")))
+  
+  #-- Dates of games within upcoming games title
+  date <- webpage %>% html_element("tbody") %>% html_elements(".day.short") %>% html_text()
+  
+  #-- Number of teams playing on today's date is 2 x number of games occurring today
+  number_of_teams <- 2 * sum(short_date == date)
+  
+  #-- Get Team Names
+  team_name <- webpage %>% html_element("tbody") %>% html_elements(".team-name.long") %>% html_text()
+  team_name <- team_name[1:number_of_teams]
+  
+  #-- Get Win Probabilities
+  chance <- webpage %>% html_element("tbody") %>% html_elements(".win-prob") %>% html_text()
+  chance <- chance[1:number_of_teams]
+  chance <- as.numeric(sub("%","",chance)) / 100
+  
+  #-- League ID
+  league_id <- 2
+  league <- rep(league_id, length(chance))
+  
+  #-- Return Probabilities
+  mlb_win_probabilities <- data.frame(league, team_name, chance)
+  
+  return(mlb_win_probabilities)  
+  
+}
+
+#-- Return NBA Win Probabilities
+get_nba_win_probabilities <- function() {
+  
+  #-- Source URL
+  url <- "https://projects.fivethirtyeight.com/2023-nba-predictions/games/"
+  
+  webpage <- read_html(url)
+  
+  #-- Include only today's data
+  today <- webpage %>% html_nodes(".day")
+  today <- today[1]
+  
+  #-- Fetch Win Percentages
+  chance <- today %>% html_nodes("tr.team") %>% html_elements(".chance") %>% html_text()
+  chance <- as.numeric(sub("%","",chance)) / 100
+  
+  #-- Fetch Team Names
+  team_name <- today %>% html_nodes("tr.team") %>% html_elements(".team") %>% html_text()
+  
+  #-- League ID
+  league_id <- 1
+  league <- rep(league_id, length(chance))
+  
+  #-- Return Probabilities
+  nba_win_probabilities <- data.frame(league, team_name, chance)
+  
+  return(nba_win_probabilities)
+  
+}
+
+get_nba_money_lines <- function() {
+  
+  url <- "https://sportsbook.draftkings.com/leagues/basketball/nba"
+  #https://sportsbook.draftkings.com/leagues/baseball/mlb
+  
+  webpage <- read_html(url)
+  
+  #-- Fetch Team Names and remove city code
+  team_name <- webpage %>% html_nodes(".event-cell__name-text") %>% html_text()
+  team_name <- sub(".*? ","", team_name)
+  
+  #-- Fetch Money Lines
+  moneyline <- as.numeric(sub("−","-",webpage %>% html_nodes(".american.no-margin") %>% html_text()))
+  
+  #-- League ID
+  league_id <- 1
+  league <- rep(league_id, length(moneyline))
+  
+  #-- Return Probabilities
+  money_lines <- data.frame(league, team_name, moneyline)
+  
+  return(money_lines)
+  
+}
